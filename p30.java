2f54a697ed8b94893726224a157995b6f9258f9a
wei <hefjtyuiwq2014@gmail.com>
2023-11-02 03:02:50 +0800

    first commit

---
+++ p30.java
@@ -0,0 +1,15 @@
+public class p30
+{
+    public static void main(String[] args)
+    {   
+        int i,j;
+
+       for(i=0;i<5;i++)
+       {
+        for(j=0;j<3;j++)
+        {
+            System.out.println("i圈是"+i+":j圈是"+j);
+        }
+       }
+    }    
+}
